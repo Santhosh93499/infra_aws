7b8884096f8999238d2d91038663fc2912008e71
santhosh <santhosh93499@gmail.com>
2025-05-16 00:10:25 +0100

    rectified s3 error name

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "devops-directive-tf-state" # REPLACE WITH YOUR BUCKET NAME
+  bucket        = "santhosh9349-tf-state"
   force_destroy = true
 }
 
