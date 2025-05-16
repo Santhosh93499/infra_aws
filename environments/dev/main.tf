9d18e05ce1c1f082c3ee18096b25203b0818800e
santhosh <santhosh93499@gmail.com>
2025-05-16 19:00:58 +0100

    rectifying s3 creation

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -13,19 +13,26 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "santhosh9349-tf-state"
+  bucket        = "santhosh9349-tf-state-unique-12345" # Change to a unique name
   force_destroy = true
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "dev"
+  }
 }
 
 resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
+
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket        = aws_s3_bucket.terraform_state.bucket 
+  bucket = aws_s3_bucket.terraform_state.id
+
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -37,8 +44,14 @@ resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-state-locking"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+
   attribute {
     name = "LockID"
     type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform State Lock Table"
+    Environment = "dev"
   }
 }
