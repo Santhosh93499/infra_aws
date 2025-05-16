e29bf73f6ebc0dcc0311d3370120fefdc8b8080e
santhosh <santhosh93499@gmail.com>
2025-05-16 22:56:56 +0100

    fixing sysntax error

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,12 +1,6 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket        = "santhosh9349-tf-state"
-  bucket        = "santhosh9349-tf-state"
   force_destroy = true
-
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = "dev"
-  }
 
   tags = {
     Name        = "Terraform State Bucket"
@@ -17,15 +11,12 @@ resource "aws_s3_bucket" "terraform_state" {
 resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
 
-
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket = aws_s3_bucket.terraform_state.id
-
   bucket = aws_s3_bucket.terraform_state.id
 
   rule {
@@ -40,15 +31,9 @@ resource "aws_dynamodb_table" "terraform-state-locking" {
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
-
   attribute {
     name = "LockID"
     type = "S"
-  }
-
-  tags = {
-    Name        = "Terraform State Lock Table"
-    Environment = "dev"
   }
 
   tags = {
